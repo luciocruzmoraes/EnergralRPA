e905314bdc17228431bfdfb56878edc52bea9d7c
Lúcio Moraes <114965759+luciocruzmoraes@users.noreply.github.com>
2025-07-02 11:34:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print ("Hello Word")
+from services.firebaseConnection_service import connectionToFirebase
+from controllers.log_controller import log
+
+#Running Main
+log.info("Running Main")
+if name == "main":
+    #Calling connectionToFireBase function from services.firebaseConnection_service
+    log.info("Main: Calling connectionToFireBase")
+    connectionToFirebase()
