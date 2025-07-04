d02859efbbe372423f08d7766cbae0fd67a29267
Luís <77413441+Luis-Spessoto@users.noreply.github.com>
2025-07-04 11:21:53 -0300

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from services.firebaseConnection_service import connectionToFirebase
-from controllers.log_controller import log
-
-#Running Main
-log.info("Running Main")
-if name == "main":
-    #Calling connectionToFireBase function from services.firebaseConnection_service
-    log.info("Main: Calling connectionToFireBase")
-    connectionToFirebase()
